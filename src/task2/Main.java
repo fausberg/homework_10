7e502f556d7ec79a19a4142dc587eeb684069956
fausberg <ivanfadeev2003@mail.ru>
2022-10-02 23:25:59 +0300

    task2

--- src/task2/Main.java
+++ src/task2/Main.java
@@ -13,19 +13,21 @@ public class Main {
             schet++;
         }
         System.out.println(schet);
+        strcopy = str;
+        System.out.println(strcopy);
 
-        String str1 = str.substring(0,str.indexOf(" "));
-        str = str.substring(str.indexOf(" ")+1);
+        String str1 = strcopy.substring(0,strcopy.indexOf(" "));
+        strcopy = strcopy.substring(strcopy.indexOf(" ")+1);
         int min = str1.length();
         String copy = str1;
 
         for(int i = 0; i < schet; i++){
             if(i == schet - 1){
-                str1 = str.substring(0);
-                str = null;
+                str1 = strcopy.substring(0);
+                strcopy = null;
             }else {
-                str1 = str.substring(0,str.indexOf(" "));
-                str = str.substring(str.indexOf(" ") + 1);
+                str1 = strcopy.substring(0,str.indexOf(" "));
+                strcopy = strcopy.substring(strcopy.indexOf(" ") + 1);
             }
             if(min >= str1.length()){
                 min = str1.length();
